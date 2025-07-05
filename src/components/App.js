2a335e37d8b859607e298818c3d1fdad5d33dfc0
yuvraaajsingh <yuvraj2802singh@gmail.com>
2025-07-05 18:01:44 +0530

    Resolved Error

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,12 @@
 
 import React, { useState } from "react";
 import './../styles/App.css';
-import ChildComponent1 from "./ChildComponent1";
-import ChildComponent2 from "./ChildComponent2";
+import Parent from "./Parent";
 
 const App = () => {
-  const [selectedOption, setSelectedOption] =useState("")
+ 
   return (
-    <div className="main-box">
-        <h1>Parents component</h1>
-        <ChildComponent1 setSelectedOption={setSelectedOption}/>
-        <ChildComponent2 setSelectedOption={setSelectedOption}/>
-        <p>Selected Option: {selectedOption}</p>
-    </div>
+    <Parent/>
   )
 }
 
