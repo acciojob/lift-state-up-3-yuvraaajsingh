d64b506d40b146e204522664db1684c078d8f528
yuvraaajsingh <yuvraj2802singh@gmail.com>
2025-07-05 18:05:05 +0530

    resolved parent

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 
 import React, { useState } from "react";
 import './../styles/App.css';
-import Parent from "./Parent";
+import ChildComponent1 from "./ChildComponent1";
+import ChildComponent2 from "./ChildComponent2";
 
 const App = () => {
- 
+  const [selectedOption, setSelectedOption] =useState("")
   return (
-    <Parent/>
+    <div className="main-box parent">
+        <h1>Parents component</h1>
+        <ChildComponent1 setSelectedOption={setSelectedOption}/>
+        <ChildComponent2 setSelectedOption={setSelectedOption}/>
+        <p>Selected Option:{selectedOption}</p>
+    </div>
   )
 }
 
